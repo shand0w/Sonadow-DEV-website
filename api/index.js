b8dc0e550b32992c8d95d5905a7e3be37c2ba74f
Karol <karoltomaszewskimusic@gmail.com>
2020-05-01 12:11:43 +0200

    Update script

--- api/index.js
+++ api/index.js
@@ -1,10 +1,7 @@
 var myHeaders = new Headers();
+const obj = JSON.parse(versions);
+const versions = '{"pixelzone" : "105", "sonadowrpg" : "101"}';
 async function getVersion() {
   game = myHeaders.get('GameName')
-  if game == 'pixelzone'{
-    return '105'
-  }
-  if game == 'sonadowrpg'{
-    return '001'
-  }
+  return (obj.pixelzone)
 }
